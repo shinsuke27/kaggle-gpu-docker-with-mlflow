bfa28955447d52de5eb9f1cbca2d68dec060eb37
Shinsuke Katsumata <shin.tech27@gmail.com>
2020-07-09 11:54:23 +0000

    Initial Commit

---
+++ set_up.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common build-essential unzip python3-pip
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+pip3 install kaggle
+sudo usermod -a -G docker ${USER}
+
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# install nvidia-container-toolkit
+sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
+sudo systemctl restart docker
+
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+sudo pkill -SIGHUP dockerd
